231a88f917fef9024d8087733ee06d33bb5963fa
王丁丁 <ddwang@ishanshan.com>
2017-03-31 14:36:27 +0800

    保留两位小数

--- src/test/base/NumTest.java
+++ src/test/base/NumTest.java
@@ -19,18 +19,23 @@ public class NumTest {
 
     @Test
     public void testPercent() {
-        Double a = 141D;
-        Double b = 141D;
+        Double a = 142D;
+        Double b = 145D;
         Double percent = a/b;
         NumberFormat numberFormat = NumberFormat.getNumberInstance();
         numberFormat.setMaximumFractionDigits(4);
         numberFormat.setMinimumFractionDigits(4);
         String towDigits = numberFormat.format(percent);
         BigDecimal bigDecimal = new BigDecimal(towDigits);
+        Float parseFloat = Float.parseFloat(towDigits);
+        Double parseDouble = Double.parseDouble(towDigits);
         logger.info("a/b=" + percent);
         logger.info("保留四位小数：" + towDigits);
         logger.info("bigDecimal表示：" + bigDecimal);
-        logger.info("用百分数表示：" + Double.parseDouble(towDigits)*100 + "%");
+        logger.info("parseDouble表示：" + parseDouble);
+        logger.info("用百分数表示parseDouble表示：" + parseDouble*100 + "%");
+        logger.info("parseFloat：" + parseFloat);
+        logger.info("用百分数表示parseFloat表示：" + parseFloat*100 + "%");
         logger.info("强制保存两位小数用百分数表示：" + df.format(Double.parseDouble(towDigits)*100) + "%");
     }
 }
